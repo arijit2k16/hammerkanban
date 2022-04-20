445d68c85b6eb2bce607d1d3c1fb17a646583f07
Riya Ghosh <infoarijitdas@gmail.com>
2022-04-21 01:46:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home"
+import "./index.scss";
+
+
+
+export default function App() {
+ 
+  return (
+    <div>  
+      <Home/>
+    </div>
+  );
+}
---
+++ src/utils/store.js
@@ -0,0 +1,22 @@
+const data = {
+    lists: {
+      "list-1": {
+        id: "list-1",
+        title: "List 1",
+        cards: [],
+      },
+      "list-2": {
+        id: "list-2",
+        title: "List 2",
+        cards: [],
+      },
+      "list-3": {
+        id: "list-3",
+        title: "List 3",
+        cards: [],
+      },
+    },
+    listIds: ["list-1", "list-2", "list-3"],
+  };
+  
+  export default data;
